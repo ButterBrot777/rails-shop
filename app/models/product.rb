7f3329eeb680614f7fb686eea0b609d72c064f09
Pavel Karenda <korendos@gmail.com>
2021-04-13 15:15:27 +0300

    feat: add model connections and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true # rubocop warning: Uniqueness validation should be with a unique index.
@@ -8,4 +12,14 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png|jpeg)\z/i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
+
 end
