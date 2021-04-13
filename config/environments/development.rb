9e33c57f5e98da5b0a4457bc03dc98811a5fa0cc
Pavel Karenda <korendos@gmail.com>
2021-04-13 18:10:50 +0300

    fix: fix session cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,8 +17,8 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
-    # enable fragment caching for portions of the page
-    config.action_controller.enable_fragment_cache_logging = true
+    # enable fragment caching for portions of the page not working :(
+    # config.action_controller.enable_fragment_cache_logging = true
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
