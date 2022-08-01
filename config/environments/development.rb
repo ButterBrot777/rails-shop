e9b8b5dace0ee19b42e9b2ec03cb926d07662795
Pavel Karenda <korendos@gmail.com>
2022-08-01 17:05:07 +0300

    fix: add time stamp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     # enable fragment caching for portions of the page not working :(
-    # config.action_controller.enable_fragment_cache_logging = true
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
