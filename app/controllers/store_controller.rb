f280d149f45b54cc5f18679758355fd213ecdbd2
Pavel Karenda <korendos@gmail.com>
2022-08-03 13:54:21 +0300

    feat: add cart in sidebar

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+
   def index
     @products = Product.order(:title)
-
-    if session[:counter].nil?
-      session[:counter] = 0
-    end
-
-    session[:counter] += 1
   end
 end
